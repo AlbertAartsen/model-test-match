507b243578389ced21bfd7a80339ed2167e5f272
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-23 14:59:17 +0200

    app create

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+User.delete_all
+
+
+aaa  =User.create!( email: "aaa@user.com", password: "abcd1234", name: "aaa", admin: false)
+bbb  =User.create!( email: "bbbk@user.com", password: "abcd1234", name: "bbb", admin: false)
+ccc  =User.create!( email: "ccc@user.com", password: "abcd1234", name: "ccc", admin: false)
+ddd  =User.create!( email: "ddd@user.com", password: "abcd1234", name: "ddd", admin: false)
+eee  =User.create!( email: "eee@user.com", password: "abcd1234", name: "eee", admin: true)
